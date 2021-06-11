b16e7814ab3fa4300edcca5344468d2d64052552
lilaspourpre <irina.nikishina@mail.ru>
2021-06-11 17:07:06 +0300

    update evaluation script

--- code/evaluation/evaluate.py
+++ code/evaluation/evaluate.py
@@ -1,8 +1,10 @@
 import argparse
 import codecs
 import json
+import random
+import os
+from statistics import mean, stdev
 from collections import defaultdict
-
 
 # Consistent with Python 2
 
@@ -16,6 +18,26 @@ def read_dataset(data_path, read_fn=lambda x: x, sep='\t'):
             hypernyms = read_fn(line_split[1])
             vocab[word].append(hypernyms)
     return vocab
+
+
+def get_score_list(reference, submitted, submitted_path, k=10):
+    random.seed(42)
+    max_items_len = int(len(reference) * 0.8)
+    all_words = list(reference)
+    map_list, mrr_list = [], []
+
+    for _ in range(30):
+        random.shuffle(all_words)
+        _80_percent_words = all_words[:max_items_len]
+        smaller_reference = dict(filter(lambda x: x[0] in _80_percent_words, reference.items()))
+        mean_ap, mean_rr = get_score(smaller_reference, submitted, k=k)
+        map_list.append(mean_ap)
+        mrr_list.append(mean_rr)
+
+    write_to_file(os.path.splitext(submitted_path)[0]+"_map_scores.json", map_list)
+    write_to_file(os.path.splitext(submitted_path)[0]+"_mrr_scores.json", mrr_list)
+
+    return map_list, mrr_list
 
 
 def get_score(reference, predicted, k=10):
@@ -62,6 +84,11 @@ def compute_rr(true, predicted, k=10):
     return 0.0
 
 
+def write_to_file(path, object):
+    with open(path, 'w', encoding='utf-8') as w:
+        json.dump(object, w)
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('reference')
@@ -73,10 +100,12 @@ def main():
     if len(set(reference).intersection(set(submitted))) == 0:
         raise Exception("Reference and Submitted files have no samples in common")
     elif set(reference) != set(submitted):
-        print("Not all words are presented in your file: reference is", len(set(reference)), "submitted is",
-              len(set(submitted)))
+        print("Not all words are presented in your file", len(set(reference)), len(set(submitted)))
+
     mean_ap, mean_rr = get_score(reference, submitted, k=10)
     print("map: {0}\nmrr: {1}\n".format(mean_ap, mean_rr))
+    map_list, mrr_list = get_score_list(reference, submitted, args.predicted, k=10)
+    print("{:.3f}+-{:.3f}\t{:.3f}+-{:.3f}".format(mean(map_list), stdev(map_list), mean(mrr_list), stdev(mrr_list)))
 
 
 if __name__ == '__main__':
